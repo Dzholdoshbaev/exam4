e300289d3dc2e98270a7a657f0e11cb8a5a62959
Dzholdoshbev Chyngyz <medzholdoshbaev@gmail.com>
2024-05-18 17:01:57 +0600

    run

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import simulation.CatSimulation;
 
 public class Main {
     public static void main(String[] args) {
-
+        CatSimulation catSimulation = new CatSimulation();
+        catSimulation.runSimulation();
     }
 }
