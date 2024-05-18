756132905d3225433c2a0eaa287d425f49c5304c
Dzholdoshbev Chyngyz <medzholdoshbaev@gmail.com>
2024-05-18 17:00:22 +0600

    creatod method for users in put print list and find cat

--- src/simulation/CatSimulation.java
+++ src/simulation/CatSimulation.java
@@ -22,6 +22,66 @@ public class CatSimulation {
         cats.add(cat3);
     }
 
+    public void printCats(){
+        cats.sort((c1,c2)-> c2.getAverageLevel() - c1.getAverageLevel());
+        System.out.println("==============================================================================");
+        System.out.println("# |     Имя     | Возраст | Здоровье | Настроение | Сытость | Средний уровень");
+        for (int i = 0; i < cats.size(); i++) {
+            Cat cat = cats.get(i);
+            String isItAfterUse = cat.isAfterUse() ? "*" : "";
+            System.out.printf("%-2d| %-11s | %-7d | %-8d | %-10d | %-7d | %-13d%n",
+                    (i + 1), isItAfterUse + cat.getName(), cat.getAge(), cat.getHealth(),
+                    cat.getMood(), cat.getFullness(), cat.getAverageLevel());
+        }
+        System.out.println("==============================================================================");
+    }
+    private String userCheckEmptyInput(){
+        while (true){
+            try{
+                String current = scanner.nextLine().trim();
+                if (current.isEmpty()){
+                    throw new UserInPut();
+                }
+                return current;
+            }catch (UserInPut e){
+                System.out.println("Вы ничего не ввели ! напишите снова !");
+            }
+        }
+    }
+    private int getIntInput() {
+        while (true) {
+            try {
+                String current = userCheckEmptyInput();
+                int userChoice = Integer.parseInt(current);
+                return userChoice;
+            } catch (NumberFormatException e) {
+                System.out.print("Надо ввести число ! введите заново : ");
+            }
+        }
+    }
+    private int getChoice(int min, int max){
+        int choice;
+        while (true) {
+            try {
+                choice = getIntInput();
+                if (choice < min || choice > max) {
+                    throw new UserInPut();
+                }
+                break;
+            } catch (UserInPut e) {
+                System.out.printf("Число не должен быть меньше %s и больше чем  %s выберите между этими значениями ", min, max);
+            }
+        }
+        return choice;
+    }
+    private Cat findCatByNumber(int number) {
+        if (number >= 1 && number <= cats.size()) {
+            return cats.get(number - 1);
+        } else {
+            return null;
+        }
+    }
+
 
 
 }
